300259753423dabfddb91743c91e088e016dac34
Rabby <rabby.raziul@gmail.com>
2024-06-10 17:48:50 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/intarest_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.intarest_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+final theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 95, 1, 1),
+  ),
+);
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Interest Calculator App',
+      home: const SIForm(),
+      theme: ThemeData(
+          primaryColor: Colors.red,
+          colorScheme: const ColorScheme.dark(
+            background: Colors.black26,
+          )),
+    ),
+  );
+}
+
+class SIForm extends StatefulWidget {
+  const SIForm({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SIFormState();
+  }
+}
+
+class _SIFormState extends State<SIForm> {
+  final _currencies = ['Rupees', 'Dollars', 'Pounds', 'Taka'];
+  final _minimumPadding = 5.0;
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle = Theme.of(context).textTheme.titleMedium;
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Center(child: Text('Interest Calculator')),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        child: ListView(
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: getImageAsset(),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                style: textStyle,
+                decoration: InputDecoration(
+                  labelText: 'Principal',
+                  labelStyle: textStyle,
+                  hintText: 'Enter Principal e.g.1200',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                style: textStyle,
+                decoration: InputDecoration(
+                  labelText: 'Rate of Interest',
+                  labelStyle: textStyle,
+                  hintText: 'In Percent',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      style: textStyle,
+                      decoration: InputDecoration(
+                        labelText: 'Term',
+                        labelStyle: textStyle,
+                        hintText: 'Time in years',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: _minimumPadding * 5,
+                  ),
+                  Expanded(
+                    child: DropdownButton<String>(
+                      items: _currencies.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      value: 'Taka',
+                      onChanged: (String? newValueSelected) {
+                        //your code execute,when a menu item is selected from dropdown
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                      ),
+                      child: const Text(
+                        'Calculate',
+                        textScaleFactor: 1.5,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.redAccent,
+                      ),
+                      child: const Text(
+                        'Reset',
+                        textScaleFactor: 1.5,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(_minimumPadding * 2),
+              child: Text('Todo Text', style: textStyle),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getImageAsset() {
+    AssetImage assetImage = const AssetImage('images/bank.png');
+    Image image = Image(
+      image: assetImage,
+      width: 400.0,
+      height: 270.0,
+    );
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumPadding),
+    );
+  }
+}
